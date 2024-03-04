5f85b9b49e922da0cd589f718c44737906cea65b
Wade Green <61675550+TrialAndErrror@users.noreply.github.com>
2024-03-03 22:43:22 -0500

    Update rc.lua
    let nitrogen handle wallpaper stuff

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -170,9 +170,6 @@ local tasklist_buttons = gears.table.join(
                                               awful.client.focus.byidx(-1)
                                           end))
 
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
