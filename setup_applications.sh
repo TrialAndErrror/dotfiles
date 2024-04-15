d700498330e720156a9924b7f19069ab70c310d1
Trial and Errror <coding@trialanderrror.com>
2024-04-14 22:19:34 -0400

    Update readme

--- setup_applications.sh
+++ setup_applications.sh
@@ -11,11 +11,11 @@ echo "Installing utilities"
 nix profile install nixpkgs#mu nixpkgs#dua nixpkgs#nushell
 
 echo "Installing formatters and linters"
-nix profile install nixpkgs#nixfmt nixpkgs#isort nixpkgs#python311Packages.pytest nixpkgs#python311Packages.nose
+nix profile install nixpkgs#nixfmt-classic nixpkgs#isort nixpkgs#python311Packages.pytest nixpkgs#python311Packages.nose
 
 
 echo "Installing version control tools"
 nix profile install nixpkgs#gh nixpkgs#jujutsu
 
 # Applications
-[ -n "$INSTALL_UNFREE" ] && nix profile install nixpkgs#jetbrains.pycharm-professional nixpkgs#jetbrains.datagrip nixpkgs#gitkraken
+[ -n "$INSTALL_UNFREE" ] && export NIXPKGS_ALLOW_UNFREE=1 && nix profile install --impure nixpkgs#jetbrains.pycharm-professional nixpkgs#jetbrains.datagrip nixpkgs#gitkraken
