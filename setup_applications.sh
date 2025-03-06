a7aca54e53ee1add00a907611988bea8926faa72
Wade Green <thewadegreen@gmail.com>
2025-03-05 21:39:17 -0500

    Update installation

--- setup_applications.sh
+++ setup_applications.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-INSTALL_UNFREE=1
-
 # Devtools
 
+install() {
+    nix profile install nixpkgs#
+}
 echo "Installing file system tools..."
-nix profile install nixpkgs#bat nixpkgs#fd nixpkgs#eza nixpkgs#pipenv
+install bat        # https://github.com/sharkdp/bat
+install fd         # https://github.com/sharkdp/fd
+install ripgrep    # https://github.com/BurntSushi/ripgrep
+install eza        # https://github.com/eza-community/eza
+install pyenv      # https://github.com/pyenv/pyenv
+install zellij     # https://zellij.dev/
 
 echo "Installing utilities"
-nix profile install nixpkgs#mu nixpkgs#dua nixpkgs#nushell
+install nushell    # https://www.nushell.sh/
+install httpie     # https://httpie.io/docs/cli
 
 echo "Installing formatters and linters"
-nix profile install nixpkgs#nixfmt-classic nixpkgs#isort nixpkgs#python311Packages.pytest nixpkgs#python311Packages.nose
+install nixfmt-classic    # https://github.com/NixOS/nixfmt
+install isort             # https://github.com/NixOS/nixfmt
+install pyright           # https://github.com/NixOS/nixfmt
 
+echo "Installing language utilities"
+install yarn                         # https://yarnpkg.com/
+install python311Packages.pytest     # https://docs.pytest.org/en/stable/
+install python311Packages.nose       # https://nose.readthedocs.io/en/latest/testing.html
+install python311Packages.black      # https://black.readthedocs.io/en/stable/
 
 echo "Installing version control tools"
-nix profile install nixpkgs#gh nixpkgs#jujutsu
+install gh      # https://cli.github.com/
 
+install_unfree() {
+    [ -n "$INSTALL_UNFREE" ] && export NIXPKGS_ALLOW_UNFREE=1 && nix profile install --impure nixpkgs#$1
+}
 # Applications
-[ -n "$INSTALL_UNFREE" ] && export NIXPKGS_ALLOW_UNFREE=1 && nix profile install --impure nixpkgs#jetbrains.pycharm-professional nixpkgs#jetbrains.datagrip nixpkgs#gitkraken
+echo "Installing IDEs"
+install_unfree jetbrains.pycharm-professional    # https://www.jetbrains.com/pycharm/
+install_unfree jetbrains.datagrip                # https://www.jetbrains.com/datagrip/
+install_unfree jetbrains.webstorm                # https://www.jetbrains.com/webstorm/
+install_unfree jetbrains.goland                  # https://www.jetbrains.com/goland/
+install_unfree gitkraken                         # https://www.gitkraken.com/
--- setup_configs.sh
+++ setup_configs.sh
@@ -1,7 +1,8 @@
-# Install required dependencies
+# Install required dependencies for Debian-based systems
 sudo nala install awesome compton nitrogen xfce4-terminal rofi arandr
 
 # Download and install config files
+mkdir -p ~/.config/awesome/
 
 [ -f ~/.config/awesome/rc.lua ] && mv ~/.config/awesome/rc.lua ~/.config/awesome/rc.lua.old
 wget -P ~/.config/awesome/ https://raw.githubusercontent.com/TrialAndErrror/dotfiles/main/.config/awesome/rc.lua
@@ -9,8 +10,15 @@ wget -P ~/.config/awesome/ https://raw.githubusercontent.com/TrialAndErrror/dotf
 [ -f ~/.config/awesome/theme.lua ] && mv ~/.config/awesome/theme.lua ~/.config/awesome/theme.lua.old
 wget -P ~/.config/awesome/ https://raw.githubusetheme.com/TrialAndErrror/dotfiles/main/.config/awesome/theme.lua
 
+[ -f ~/.config/awesome/autorun.sh ] && mv ~/.config/awesome/autorun.sh ~/.config/awesome/autorun.sh.old
+wget -P ~/.config/awesome/ https://raw.githubusetheme.com/TrialAndErrror/dotfiles/main/.config/awesome/autorun.sh
+
 [ -f ~/.config/rofi/config.rasi ] && mv ~/.config/rofi/config.rasi ~/.config/rofi/config.rasi.old
 wget -P ~/.config/rofi/ https://raw.githubusercontent.com/TrialAndErrror/dotfiles/main/.config/rofi/config.rasi
 
 [ -f ~/.config/xfce4/terminal/terminalrc ] && mv ~/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc.old
 wget -P ~/.config/xfce4/terminal https://raw.githubusercontent.com/TrialAndErrror/dotfiles/main/.config/xfce4/terminal/terminalrc
+
+
+[ -f ~/.config/doom/ide-settings.el ] && mv ~/.config/doom/ide-settings.el ~/.config/doom/ide-settings.el.old
+wget -P ~/.config/doom/ https://raw.githubusercontent.com/TrialAndErrror/dotfiles/main/.config/doom/ide-settings.el
